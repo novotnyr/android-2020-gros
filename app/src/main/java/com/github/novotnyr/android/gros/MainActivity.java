0d4ff793507e0643e0f07ed03371d5adec5acf49
Robert Novotny <novotnr0@gmail.com>
2020-04-05 13:08:09 +0200

    Pridana obsluha runtime prav

--- app/src/main/java/com/github/novotnyr/android/gros/MainActivity.java
+++ app/src/main/java/com/github/novotnyr/android/gros/MainActivity.java
@@ -1,11 +1,17 @@
 package com.github.novotnyr.android.gros;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import static android.Manifest.permission.SEND_SMS;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_SMS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +20,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onPayButtonClick(View view) {
+        if (ActivityCompat.checkSelfPermission(this, SEND_SMS) != PERMISSION_GRANTED) {
+            String[] permissions = {SEND_SMS};
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_SMS);
+        } else {
+            // odošli SMS
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_SMS:
+                if (grantResults.length == 0 || grantResults[0] != PERMISSION_GRANTED ) {
+                    // Neboli udelené žiadne oprávnenia, alebo dialóg pre potvrdenie bol zrušený
+                    return;
+                }
+                // odošli SMS
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
